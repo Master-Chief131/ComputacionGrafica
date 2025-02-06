64d1ea3f2ac5e2964daac572d0d2c9b5f735400b
Albert~M <105120358+Master-Chief131@users.noreply.github.com>
2025-02-05 20:26:23 -0500

    Add initial project files, including HTML, JavaScript, and material files; configure .gitignore for node_modules

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+var controlAudio = document.getElementById('Toque');
+var audio = document.getElementById('mySong');
+var audioPlaying = false;
+var firstPlay = true;
+var currentTime = 0;
+
+document.body.addEventListener('click', function() {
+   if (!audioPlaying) {
+      if (firstPlay) {
+         audio.currentTime = 29;
+         firstPlay = false;
+      }
+      audio.play().then(function() {
+         // Reproducción exitosa
+         console.log('Audio reproduciéndose');
+         audioPlaying = true;
+         controlAudio.textContent = '';
+      }).catch(function(error) {
+         // Manejar errores de reproducción
+         console.error('Error al reproducir audio:', error);
+      });
+   } else {
+      audio.pause();
+      audioPlaying = false;
+      controlAudio.textContent = '';
+   }
+});
